a93e041d300858e96d8c24022921746c39eb2562
ayushrai1901 <raiayush1901@gmail.com>
2023-09-18 10:54:57 +0530

    How to check Positive or negative number

---
+++ positiveNegative.java
@@ -0,0 +1,11 @@
+//Check if a Number is Positive or Negative in Java
+
+/*N > 0 then, number is Positive.
+N < 0 then, number is Negative.
+N = 0 then, number is Zero. */
+
+/*To solve the above problem we write a Java code using three different methods.
+
+Method 1: Using Brute Force
+Method 2: Using Nested if-else Statements
+Method 3: Using Ternary Operators */
