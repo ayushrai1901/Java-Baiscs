23a689e63a4cb832ce49816ec79a397fa566440b
ayushrai1901 <raiayush1901@gmail.com>
2023-10-05 22:02:26 +0530

    Power of Num

--- Recursion/powerOfNum.java
+++ Recursion/powerOfNum.java
@@ -25,7 +25,15 @@ import javax.print.DocFlavor.STRING;
 public class powerOfNum{
     public static void main(String [] args){
         int base =5, x=3;
-        
+        System.out.println("Required Power is " + power(base, x));
+    }
+    static int power(int base, int x) {
+        int result = 1;
+        while (x > 0) {
+            result *= base;
+            x--;
+        }
+        return result;
     }
 }
 
