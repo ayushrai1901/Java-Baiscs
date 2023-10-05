fadddd2a0ee69fb2f43c8e0c009a5a6b92687f9c
ayushrai1901 <raiayush1901@gmail.com>
2023-10-05 22:02:12 +0530

    Power of Number

---
+++ Recursion/powerOfNum.java
@@ -0,0 +1,31 @@
+package Recursion;
+
+import java.security.PublicKey;
+
+import javax.print.DocFlavor.STRING;
+
+/*  public class powerOfNum{
+    public static void main(String[] args){
+        int base = 5, x=3;
+        System.out.println("Required Power is "+power(base,x));
+    }
+
+    //recursive function
+    static int power(int base, int x){
+        if(x==0){
+            return 1;
+        }
+        return (base*power(base, x-1));
+    }
+}*/
+
+
+//using for loop
+
+public class powerOfNum{
+    public static void main(String [] args){
+        int base =5, x=3;
+        
+    }
+}
+
